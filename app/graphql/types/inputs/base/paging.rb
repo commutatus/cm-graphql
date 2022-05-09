531142950de208d231f1111dfecbf7d5ea9bcee4
DeboDevelop <debajyotidofficial@gmail.com>
2022-05-09 15:22:02 +0530

    Added pagination in graphql

---
+++ app/graphql/types/inputs/base/paging.rb
@@ -0,0 +1,14 @@
+module Types
+    module Inputs
+      module Base
+        class Paging < Types::BaseInputObject
+          graphql_name "PagingInput"
+  
+          description "Attributes needed for paginating list of items"
+  
+          argument :page_no,      Integer, nil,  required: true
+          argument :per_page,     Integer, nil,  required: false
+        end
+      end
+    end
+  end
---
+++ app/graphql/types/objects/base/paging_type.rb
@@ -0,0 +1,9 @@
+module Types::Objects::Base
+    class PagingType < Types::BaseObject
+        graphql_name "PagingType"
+        field :current_page, Integer, null: true
+        field :total_items, Integer, null: true
+        field :total_pages, Integer, null: true
+        field :total_count, Integer, null: true
+    end
+end
