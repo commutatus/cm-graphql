a3824aa333e25d60243e79ecf2cc27b385e8a4e4
Michael Kevin Victor <me@mikevictor.me>
2026-02-16 19:35:13 -0500

    improvement: simplify and add test classes

--- app/graphql/cm_graphql/extensions/recaptcha_extension.rb
+++ app/graphql/cm_graphql/extensions/recaptcha_extension.rb
@@ -1,22 +1,16 @@
-require_relative 'input_without_recaptcha_token'
-
 module CmGraphql
   module Extensions
     class RecaptchaExtension < GraphQL::Schema::FieldExtension
       def apply
-        input_arg = field.arguments&.dig(:input) || field.arguments&.dig("input")
-        input_type = unwrap_type(input_arg&.type)
-
-        if input_type&.respond_to?(:argument)
-          input_type.argument(:recaptcha_token, String, required: true)
-        else
-          field.argument(:recaptcha_token, String, required: true)
-        end
+        input_arg = field.arguments["input"]
+        input_type = input_arg.type.unwrap
+        input_type.argument(:recaptcha_token, String, required: true)
       end
- 
+
       def resolve(object:, arguments:, context:, **_rest)
-        ruby_kwargs = arguments.respond_to?(:keyword_arguments) ? arguments.keyword_arguments : arguments
-        recaptcha_token = extract_recaptcha_token(ruby_kwargs[:input])
+        args_hash = arguments.to_h
+        input_hash = args_hash[:input].to_h
+        recaptcha_token = input_hash[:recaptcha_token]
 
         RecaptchaVerifier.verify_v3!(
           token: recaptcha_token,
@@ -25,57 +19,8 @@ module CmGraphql
           minimum_score: options[:minimum_score] || RECAPTCHA_MINIMUM_SCORE
         )
 
-        forwarded_kwargs = ruby_kwargs.dup
-        forwarded_kwargs[:input] = sanitize_input(forwarded_kwargs[:input])
-        forwarded_kwargs.delete(:recaptcha_token)
-
-        yield(object, forwarded_kwargs, nil)
-      end
-
-      private
-
-      def extract_recaptcha_token(ruby_kwargs)
-        input = ruby_kwargs[:input]
-        input_hash = input_to_hash(input)
-
-        if input_hash.is_a?(Hash)
-          input_hash[:recaptcha_token] || input_hash["recaptcha_token"]
-        else
-          ruby_kwargs[:recaptcha_token]
-        end
-      end
-
-      def sanitize_input(input)
-        return nil if input.nil?
-        return CmGraphql::Extensions::InputWithoutRecaptchaToken.new(input) if input.respond_to?(:to_kwargs)
-
-        input_hash = input_to_hash(input)
-        return input_hash unless input_hash.is_a?(Hash)
-
-        sanitized = input_hash.dup
-        sanitized.delete(:recaptcha_token)
-        sanitized
-      end
-
-      def input_to_hash(input)
-        return nil if input.nil?
-
-        if input.respond_to?(:to_kwargs)
-          kwargs = input.to_kwargs
-          kwargs.respond_to?(:to_h) ? kwargs.to_h : nil
-        elsif input.respond_to?(:keyword_arguments)
-          input.keyword_arguments
-        elsif input.respond_to?(:to_h)
-          input.to_h
-        elsif input.is_a?(Hash)
-          input
-        end
-      end
-
-      def unwrap_type(type)
-        return nil unless type
-        type = type.of_type while type.respond_to?(:of_type)
-        type
+        next_input = input_hash.except(:recaptcha_token)
+        yield(object, args_hash.except(:input).merge(input: next_input), nil)
       end
     end
   end
